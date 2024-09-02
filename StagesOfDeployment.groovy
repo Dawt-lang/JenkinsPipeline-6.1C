e53290ad9ed094bb85f4b3bb38dbff45c14364a9
DrebbWaffle <s223361641@deakin.edu.au>
2024-09-02 16:13:14 +1000

    Add JenkinsFile
    Add a jenkins file for 6.1C
    
    Make sure you can sent post requests and emails, to be used in Jenkins Pipeline.

---
+++ StagesOfDeployment.groovy
@@ -0,0 +1,53 @@
+pipeline {
+    agent any
+    environment {
+        DIRECTORY_PATH= "C:/ProgramData/Jenkins/.jenkins/workspace"
+        TESTING_ENVIRONMENT= "C:/ProgramData/Jenkins/.jenkins/workspace/first_pipe@2"
+        PRODUCTION_ENVIRONMENT= "Dawt"
+    }
+    
+    stages {
+        stage('Build') {
+            steps {
+                echo 'Tool to be used: Maven'
+                echo 'build the code using a build automation tool, package code.'
+            }
+        }
+        stage('Unit And Integration Checks') {
+            steps {
+                echo 'Tool to be used: Katalon Studio'
+                echo 'run unit tests to ensure the code functions as expected and run integration tests to ensure the different components of the application work together as expected'
+            }
+        }
+        stage('Code Analysis') {
+            steps {
+                echo 'Tool to be used: Crucible'
+                echo 'integrate a code analysis tool to analyse the code and ensure it meets industry standards'
+            }
+        }
+        stage('Security Scan') {
+            steps {
+                echo 'Tool to be used: SonarQube'
+                echo 'perform a security scan on the code using a tool to identify any vulnerabilities'
+            }
+        }
+        stage('Deploy To Staging') {
+            steps {
+                echo 'Tool to be used: AWS EC2 Instance'
+                echo 'deploy the application to a staging server'
+            }
+        }
+        stage('Integration Tests On Staging') {
+            steps {
+                echo 'Tool to be used: Github'
+                echo 'run integration tests on the staging environment to ensure the application functions as expected in a production-like environment.'
+            }
+        }
+        stage('Deploy To Production') {
+            steps {
+                echo 'Tool to be used: AWS EC2 Instance'
+                echo 'Deploy to Production Server (AWS Ec2 instance)'
+            }
+        }
+    }
+}
